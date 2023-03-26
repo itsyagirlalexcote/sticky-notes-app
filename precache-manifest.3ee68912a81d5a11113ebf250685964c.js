aa5608fd5fd817931e74d5b6cb2241e21ab37b5a
Alexandra Côté-Lahue <alexcotelahue@gmail.com>
2023-03-26 15:24:08 -0400

    Updates

---
+++ precache-manifest.3ee68912a81d5a11113ebf250685964c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1255d62dc7c5b882b4e2",
+    "url": "/sticky-notes-app/static/css/main.9e0351f8.chunk.css"
+  },
+  {
+    "revision": "1255d62dc7c5b882b4e2",
+    "url": "/sticky-notes-app/static/js/main.7c84408a.chunk.js"
+  },
+  {
+    "revision": "c1f5a75c2ca3b682f1b7",
+    "url": "/sticky-notes-app/static/js/runtime~main.11529745.js"
+  },
+  {
+    "revision": "aacd201bf72820a96c02",
+    "url": "/sticky-notes-app/static/js/2.b4876291.chunk.js"
+  },
+  {
+    "revision": "19592199a9b9edbbe3c6421fb684353b",
+    "url": "/sticky-notes-app/index.html"
+  }
+];
